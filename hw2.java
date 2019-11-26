38df069faa6a4c8f87202124f89cb5ec308dca0b
0624059 <55127466+0624059@users.noreply.github.com>
2019-11-26 16:10:36 +0800

    Update hw2.java

--- hw2.java
+++ hw2.java
@@ -32,7 +32,7 @@ public class hw2{
     public static void main(String arg[]){
         Rectangle rec=new Rectangle(10,20);
         System.out.println(rec.toString(rec.getArea));
-        Triangle rec=new Triangle(15,25);
+        Triangle tri=new Triangle(15,25);
         System.out.println(rec.toString(rec.getArea));
     }
 }
