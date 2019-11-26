ba80199f01ac029e808f70dc8bb0b5c677763443
0624059 <55127466+0624059@users.noreply.github.com>
2019-11-26 15:59:46 +0800

    Add files via upload

---
+++ hw2.java
@@ -0,0 +1,38 @@
+interface Shape{
+    abstract void getArea(double n);
+}
+
+class Rectangle implements Shape{
+    double length,width;
+    public Rectangle(double l,double w){
+        length=l;
+        width=w;
+    }
+    public double getArea(){
+        return length*width;
+    }
+    public String toString(double n){
+        return "area =" +n;
+    }
+}
+class Triangle implements Shape{
+    double base,height;
+    public Triangle(double a,double b){
+        base=a;
+        height=b;
+    }
+    public double getArea(){
+        return base*height/2;
+    }
+    public String toString(double n){
+        return "area =" +n;
+    }
+}
+public class hw2{
+    public static void main(String arg[]){
+        Rectangle rec=new Rectangle(10,20);
+        System.out.println(rec.toString(rec.getArea));
+        Triangle rec=new Triangle(15,25);
+        System.out.println(rec.toString(rec.getArea));
+    }
+}
